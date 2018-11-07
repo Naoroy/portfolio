8b88ecdde3b59856476274fbb087cd3c6eb66125
Fégan PIERRE <fegan.pierre@gmail.com>
2018-11-07 09:06:32 +0100

    Create resume component and add pdf.js to display resume

---
+++ src/components/Home/resume.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div id="resume">
+    <div class="container--flex-column">
+      <a :href="resume" target="_blank"> My resume </a>
+      <embed class="pdf box--center" :src="resume" width="600" height="500" alt="pdf" pluginspage="http://www.adobe.com/products/acrobat/readstep2.html">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data:() => ({
+    resume: './src/components/Home/assets/resume.pdf'
+  })
+}
+</script>
+
+<style lang="scss">
+.container--flex-column {
+  display: flex;
+  flex-direction: column;
+
+  .pdf {
+    height: 100vh;
+    width: 60vw;
+  }
+
+  .box--center {
+    margin: auto;
+  }
+}
+</style>
